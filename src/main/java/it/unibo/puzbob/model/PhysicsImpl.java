9625d64931d3400b554894d9eec070a21dbbc590
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2023-08-04 19:16:15 +0200

    Corrected some bugs and added a Test for physics

--- src/main/java/it/unibo/puzbob/model/PhysicsImpl.java
+++ src/main/java/it/unibo/puzbob/model/PhysicsImpl.java
@@ -1,5 +1,9 @@
 package it.unibo.puzbob.model;
 
+/**
+ * This class calculate the physics about the shoted ball
+ */
+
 public class PhysicsImpl implements Physics {
 
     private Pair<Double, Double> boardDimension;
@@ -10,6 +14,7 @@ public class PhysicsImpl implements Physics {
         this.velocity = velocity;
     }
 
+    // This method return the ball position after some time elapsed
     public Pair<Double, Double> getBallPosition(FlyingBallImpl flyingBall, int cannonAngle,
             double time) {
 
@@ -18,57 +23,67 @@ public class PhysicsImpl implements Physics {
 
     private Pair<Double, Double> calcPosition(Pair<Double, Double> startingPosition, int cannonAngle,
             double time, boolean positive, double halfBallDimension) {
-        
-        double x;
-        double y;
-        double angle = Math.toRadians(cannonAngle) - (Math.PI / 2.0);
 
-        if (positive) {
-            x = startingPosition.getX() + 
-                (this.velocity * time * Math.sin(angle));
-        } else {
-            x = startingPosition.getX() -
-                (this.velocity * time * Math.sin(angle));
+        // This resolve some calc problem that accour when the ball is too close to the wall
+        if (time < 0) {
+            return startingPosition;
         }
         
-        if (x < 0 + halfBallDimension) {
-            x = 0 + halfBallDimension;
-            y = this.getYBouncing(startingPosition, angle);
-            double newTime = this.calcNewTime(x, y - startingPosition.getY(), time);
+        // The angle need to be in radians
+        double angle = Math.toRadians(cannonAngle - 90);
+        double x = startingPosition.getX();
+        double y;
 
-            // Next Row check bouncing
-            // System.out.println("x " + x + " y " + y + " time " + newTime + " positive " + positive);
+        // Positive change the direction after a bouncing
+        if (positive) {
+            x += (this.velocity * time * Math.sin(angle));
+        } else {
+            x -= (this.velocity * time * Math.sin(angle));
+        }
 
-            return this.calcPosition(new Pair<Double,Double>(x, y), cannonAngle, newTime, !positive, halfBallDimension);
-        } else if (x > this.boardDimension.getX() - halfBallDimension) {
-            x = this.boardDimension.getX() - halfBallDimension;
-            y = this.getYBouncing(startingPosition, angle);
-            double newTime = this.calcNewTime(x, y - startingPosition.getY(), time);
+        // If the ball after this time bounce
+        if (x < 0 + halfBallDimension || x > this.boardDimension.getX() - halfBallDimension) {
 
-            // Next Row check bouncing
-            // System.out.println("x " + x + " y " + y + " time " + newTime + " positive " + positive);
+            // Put the x in one of the extreme point
+            if (x < 0 + halfBallDimension)
+                 x = 0 + halfBallDimension;
+            else if (x > this.boardDimension.getX() - halfBallDimension)
+                 x = this.boardDimension.getX() - halfBallDimension;
 
-            return this.calcPosition(new Pair<Double,Double>(x, y), cannonAngle, newTime, !positive, halfBallDimension);
-        } 
+            // getYBouncing calc the y coordinate
+            y = this.getYBouncing(startingPosition, angle, x);
 
+            // newtime is the time left to the movement
+            double newTime = this.calcNewTime(Math.abs(x - startingPosition.getX()),
+                y - startingPosition.getY(), time);
+
+            // Call this function recursively
+            return this.calcPosition(new Pair<Double,Double>(x, y), 
+                cannonAngle, newTime, !positive, halfBallDimension);
+        }
+
+        // If is not recursive, calc the y coordinate
         y = startingPosition.getY() + 
             (this.velocity * time * Math.cos(angle));
 
+        // Return the result
         return new Pair<Double,Double>(x, y);
     }
 
-    private double getYBouncing(Pair<Double, Double> startingPosition, double angle) {
-        if (startingPosition.getX() == (this.boardDimension.getX() / 2)) {
-            return startingPosition.getY() + Math.abs((0.5 * this.boardDimension.getX() * (Math.cos(angle) / Math.sin(angle))));
-        } else {
-            return startingPosition.getY() + Math.abs(this.boardDimension.getX() * (Math.cos(angle) / Math.sin(angle)));
-        }
+    // Calc the y when the ball bounces
+    private double getYBouncing(Pair<Double, Double> startingPosition, double angle, double x) {
+
+        return startingPosition.getY() + (Math.abs(x - startingPosition.getX()) * Math.cos(angle));
+
     }
 
+    // Calc the time left to the movement after the bounce
     private double calcNewTime(double x, double y, double time) {
+
         double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
         double timeToSubtract = distance / this.velocity;
         return time - timeToSubtract;
+
     }
     
 }
---
+++ src/test/java/it/unibo/puzbob/model/PhysicsTest.java
@@ -0,0 +1,71 @@
+package it.unibo.puzbob.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhysicsTest {
+
+    Physics fisica = new PhysicsImpl(new Pair<Double,Double>(15.0, 10.0), 5);
+
+    FlyingBallImpl ball2 = new FlyingBallImpl("WHITE", 3, 0.5, new Pair<Double,Double>(7.5, 0.0));
+
+    @Test
+    void positioningTest() {
+        // Shot without bounce 25°
+        assertEquals(2.968, 
+            fisica.getBallPosition(ball2, 25, 1).getX(), 
+            0.2, 
+            "Not the result expected");
+
+        assertEquals(2.113, 
+            fisica.getBallPosition(ball2, 25, 1).getY(), 
+            0.2, 
+            "Not the result expected");
+        
+        // Shot without bounce 165°
+        assertEquals(12.330, 
+            fisica.getBallPosition(ball2, 165, 1).getX(), 
+            0.2, 
+            "Not the result expected");
+
+        assertEquals(1.294, 
+            fisica.getBallPosition(ball2, 165, 1).getY(), 
+            0.2, 
+            "Not the result expected");
+
+        // Shot with 1 bounce 25°
+        assertEquals(2.017, 
+            fisica.getBallPosition(ball2, 25, 2).getX(), 
+            0.2, 
+            "Not the result expected");
+
+        assertEquals(3.961, 
+            fisica.getBallPosition(ball2, 25, 2).getY(), 
+            0.3, 
+            "Not the result expected");
+
+        // Shot with 1 bounce 150°
+        assertEquals(13.109, 
+            fisica.getBallPosition(ball2, 150, 2).getX(), 
+            0.3, 
+            "Not the result expected");
+
+        assertEquals(4.446, 
+            fisica.getBallPosition(ball2, 150, 2).getY(), 
+            0.3, 
+            "Not the result expected");
+        
+        // Shot with 2 bounces 25°
+        assertEquals(12.374, 
+            fisica.getBallPosition(ball2, 45, 6).getX(), 
+            0.3, 
+            "Not the result expected");
+
+        assertEquals(17.060, 
+            fisica.getBallPosition(ball2, 45, 6).getY(), 
+            1, 
+            "Not the result expected");
+
+    }
+    
+}

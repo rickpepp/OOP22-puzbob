c12100cc9ec4e164572dc102c95d6306b134cd4c
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2023-08-06 23:15:18 +0200

    Corrected some damage in git branch feature-gameloop

--- src/main/java/it/unibo/puzbob/view/Output.java
+++ src/main/java/it/unibo/puzbob/view/Output.java
@@ -3,8 +3,8 @@ package it.unibo.puzbob.view;
 import org.json.JSONObject;
 
 /**
- * This is a generic interface for a view class. In input has a JSONObject that show
- * the world status at the moment.
+ * This is a basic interface for an output source. It take in input a JSONObject that contain the information
+ * about the state of the actual world. It is done in json to have a separated view and model.
  */
 
 public interface Output {

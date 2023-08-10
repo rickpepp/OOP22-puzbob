0811aab3986d50cbdb657163da655f6a297a531f
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2023-08-10 23:52:52 +0200

    Added some information ad the readme and a license. Added an icon for application. The game window is not resizeble

--- src/main/java/it/unibo/puzbob/view/View.java
+++ src/main/java/it/unibo/puzbob/view/View.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
 public class View extends Application {
@@ -13,21 +14,40 @@ public class View extends Application {
     public static final double WINDOW_PROPORTION = 1.5;
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     private final String FXML_FOLDER = "view" + FILE_SEPARATOR + "board.fxml";
-    private static final String APP_NAME = "Puzzle Bobble";
+    private static final String APP_NAME = "Puzbob";
+
+    private final String ICON16 = "view" + FILE_SEPARATOR + "icon16.png";
+    private final String ICON32 = "view" + FILE_SEPARATOR + "icon32.png";
+    private final String ICON64 = "view" + FILE_SEPARATOR + "icon64.png";
 
     private static Output output;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // Get screen Dimension
         Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Load the fxml file
         FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource(FXML_FOLDER));
         AnchorPane rootPane = loader.load();
         final Scene scene = new Scene(rootPane, screenSize.getWidth() / WINDOW_PROPORTION, 
             screenSize.getHeight() / WINDOW_PROPORTION);
+
+        // Get Controller
         FXMLController fxmlController = loader.getController();
         output = fxmlController.getOutput();
+
+        // The window is not resizable
+        primaryStage.setResizable(false);
+
+        // Icons of the application
+        Image icon16 = new Image(ClassLoader.getSystemResource(ICON16).toString());
+        Image icon32 = new Image(ClassLoader.getSystemResource(ICON32).toString());
+        Image icon64 = new Image(ClassLoader.getSystemResource(ICON64).toString());
+        primaryStage.getIcons().addAll(icon16, icon32, icon64);
         
-        
+        // Property of stage
         primaryStage.setTitle(APP_NAME);
         primaryStage.setScene(scene);
         primaryStage.show();
